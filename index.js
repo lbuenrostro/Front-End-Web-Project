5c424a9c8fc2d074b164c4555c855e3ab8ca1e74
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-11-16 10:05:54 -0600

    Made a Page_data to display all items, One is in stock

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+var PAGE_DATA = {
+    items: {
+        img: 'solo3.jpg',
+        Name: 'Beat SoloX3',
+        Description:
+            'Ultra Violet, with up to 40 hours of battery life,' +
+            '<br/' +
+            'Beats SoloX3 Wireless is your perfect everyday headphone.<br/>Get most out of your music and award-winning, emotionally <br/> charged Beats listening experience',
+        Price: '$299.95'
+    }
+};
+
+function loaditemsInfo() {
+    var items = PAGE_DATA.items;
+
+    var html = '<img id="ppic" src="' + items.img + '">';
+    html += '<h3>' + items.Name + '</h3>';
+    html += '<p>' + items.Description + '</p>';
+    html += '<p>' + items.Price + '<p>';
+    $('#personaldata').html(html);
+    $('#items-pic').attr('src', items.img);
+}
+
 function main() {
-    $('#game-over').html(
-        'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
-    );
+    loaditemsInfo();
 }
 
 $(main);
+// function main() {
+//     $('#game-over').html(
+//         'Game Over <i class="fa fa-gamepad" aria-hidden="true"></i>'
+//     );
+// }
+
+// $(main);
