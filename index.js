c9f4a28d58f506360663abacebdb08a3f025d362
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2017-11-17 13:39:38 -0600

    added 'button:' in my Page_Data so they would work separately

--- index.js
+++ index.js
@@ -4,29 +4,57 @@ var PAGE_DATA = {
             img: 'studio.png',
             Name: 'Beats STUDIO3 Wireless',
             Description:
-                'Ultra Violet, Apple W1 chip, Wireless Bluetooth,<br>Pure Adaptive Noise Cancelling actively blocks external noise<br>With Fast Fuel, a 10-minute charge gives 3 hours of play when battery is low        ',
-            Price: '$349.95'
+                'Apple W1 chip, Wireless Bluetooth,<br>Pure Adaptive Noise Cancelling actively blocks external noise<br>With Fast Fuel, a 10-minute charge gives 3 hours of play when battery is low        ',
+            Price: '$349.95',
+            button:
+                '<button type="button" id="btn3" class="btn btn-default">' +
+                '<i class="fa fa-apple" aria-hidden="true"></i>Purchase' +
+                '</button>' +
+                '<br/>' +
+                '<small id="btn3" class="form-text text-muted">' +
+                'Taxes & shipping included</small>'
         },
         {
             img: 'pro.png',
             Name: 'Beats Pro',
             Description:
-                'Ultra Fire, clear highs adn deep lows for premium sound quality,<br>Rotating ear cups,<br>Durable housing and cushioned headband for long hours in the studio',
-            Price: '$399.95'
+                'Clear highs adn deep lows for premium sound quality,<br>Rotating ear cups,<br>Durable housing and cushioned headband for long hours in the studio',
+            Price: '$399.95',
+            button:
+                '<button type="button" id="btn4" class="btn btn-default">' +
+                '<i class="fa fa-apple" aria-hidden="true"></i>Purchase' +
+                '</button>' +
+                '<br/>' +
+                '<small id="btn4" class="form-text text-muted">' +
+                'Taxes & shipping included</small>'
         },
         {
             img: 'solo.png',
             Name: 'Beats SOLO3 Wireless',
             Description:
-                'Ultra Violet, Connect via Class 1 Bluetooth with your device for wireless listening<br>Up to 40 hours of battery life for multi-day use<br>With Fast Fuel, 5 minutes of charging gives you 3 hours of playback when battery is low',
-            Price: '$299.95'
+                'Connect via Class 1 Bluetooth with your device for wireless listening<br>Up to 40 hours of battery life for multi-day use<br>With Fast Fuel, 5 minutes of charging gives you 3 hours of playback when battery is low',
+            Price: '$299.95',
+            button:
+                '<button type="button" id="btn5" class="btn btn-default">' +
+                '<i class="fa fa-apple" aria-hidden="true"></i>Purchase' +
+                '</button>' +
+                '<br/>' +
+                '<small id="btn5" class="form-text text-muted">' +
+                'Taxes & shipping included</small>'
         },
         {
             img: 'ep.png',
             Name: 'Beats EP',
             Description:
-                'Ultra Violet, Fine-tuned acoustics for the clarity and depth you expect from Beats<br>Durable,lightweight design reinforced with stainless steel<br>Battery-free for unlimited platback<br>Adjustment vertiical sliders for a peronalized fit',
-            Price: '$129.95'
+                'Fine-tuned acoustics for the clarity and depth you expect from Beats<br>Durable,lightweight design reinforced with stainless steel<br>Battery-free for unlimited platback<br>Adjustment vertiical sliders for a peronalized fit',
+            Price: '$129.95',
+            button:
+                '<button type="button" id="btn6" class="btn btn-default">' +
+                '<i class="fa fa-apple" aria-hidden="true"></i>Purchase' +
+                '</button>' +
+                '<br/>' +
+                '<small id="btn6" class="form-text text-muted">' +
+                'Taxes & shipping included</small>'
         }
     ]
 };
@@ -35,9 +63,10 @@ function makeItem(item) {
     var html = '<img id="ppic" src="' + item.img + '">';
     html += '<h3>' + item.Name + '</h3>';
     html += '<p>' + item.Description + '</p>';
-    html += '<p>' + item.Price + '<p>';
+    html += '<p>' + item.Price + '<p>' + item.button;
     return html;
 }
+
 function loaditemsInfo() {
     var items = PAGE_DATA.items;
     var html = items
@@ -52,6 +81,22 @@ function loaditemsInfo() {
     });
     $('#btn2').click(function() {
         $('#Create2').toggle();
+    });
+    $('#btn3').on('click', function() {
+        alert('Added to your bag! Thanks for the purchase');
+        $('#btn3').attr('disabled', true);
+    });
+    $('#btn4').on('click', function() {
+        alert('Added to your bag! Thanks for the purchase');
+        $('#btn4').attr('disabled', true);
+    });
+    $('#btn5').on('click', function() {
+        alert('Added to your bag! Thanks for the purchase');
+        $('#btn5').attr('disabled', true);
+    });
+    $('#btn6').on('click', function() {
+        alert('Added to your bag! Thanks for the purchase');
+        $('#btn6').attr('disabled', true);
     });
 }
 
